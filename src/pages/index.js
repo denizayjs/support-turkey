7c8ff03e9675e9f1a6990032834127b22660d312
denizayjs <denizay@std.iyte.edu.tr>
2023-02-08 09:45:50 +0300

    change favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <title>Support Turkey</title>
         <meta name="description" content="Support Turkey" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.jpeg" />
       </Head>
 
       <main className={styles.main}>
